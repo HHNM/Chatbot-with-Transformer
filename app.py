7920bdb04a5aa6aefa16a78171e45553bda7a8f6
HHNM <94008538+HHNM@users.noreply.github.com>
2021-11-24 20:50:11 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from model import predict
+from flask import Flask, render_template, request
+
+
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return predict(userText)
+
+if __name__ == "__main__":
+    app.run()
